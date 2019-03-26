eda8a9632c664555982f18daaf7900308731b66c
user150454 <user150454@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-03-26 09:07:25 -0500

    fin séance

--- colval/colval/Controler/ParkingViewController.swift
+++ colval/colval/Controler/ParkingViewController.swift
@@ -5,24 +5,43 @@
 //  Created by user150454 on 3/18/19.
 //  Copyright © 2019 Alexandre Arsenault. All rights reserved.
 //
-
+import Foundation
 import UIKit
 import MapKit
 class ParkingViewController: UIViewController , MKMapViewDelegate{
 
 
     @IBOutlet weak var mapView: MKMapView!
+    var dc:DataControler  = DataControler.sharedInstance
+
+    // true if disponible
+    var parkingValue:[String:Bool] = [:]
+
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
  
+        let date = Date()
+       // let dateEnd = Calendar.current.date(byAdding: .day, value: 1, to: date)!
         
+        dc.getRentsForTimeRange(pStart: date, pEnd: date) { rents in
+            if rents != nil {
+            for rent in rents!{
+                
+            
+                self.parkingValue[rent.parkingID] = true
+            }
+            }else {print ("missing in parking controller")}
+            
+        }
         
+      
         let location = CLLocationCoordinate2D(latitude: 45.2523979, longitude: -74.1324644)
-        
+       
         let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.0005, longitudeDelta: 0.0005))
         
-        self.mapView.setRegion(region,animated: true)
+        self.mapView.setRegion(region,animated: false)
         
         
         
@@ -35,8 +54,14 @@ class ParkingViewController: UIViewController , MKMapViewDelegate{
             let session = URLSession.shared
             session.dataTask(with: req){
                 (data, response, error) in
+                
+                
+                
                 if let d = data {
                     let jsonFile = try? JSONSerialization.jsonObject(with: d, options: [])
+                    
+                    
+                    
                     if var json = jsonFile as? [[String: Any]] {
                         // parser le json et stocker les données dans le tableau
                         var pin2:pin
@@ -52,20 +77,35 @@ class ParkingViewController: UIViewController , MKMapViewDelegate{
                             print (pin2.coordinate)
                             self.mapView.addAnnotation(pin2)
                             
- 
+                            
                         }
 
                     }
                 }
                 }.resume()
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
     } // didload
     
 
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {  // annotation specific
         let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "customannotation")
-        annotationView.image = UIImage(named: "parking")
+       
+        
+        
+        annotationView.image = UIImage(named: "parking-empty")
+        
+        
+        
+        
         
         annotationView.canShowCallout = true
         return annotationView
@@ -77,6 +117,10 @@ class ParkingViewController: UIViewController , MKMapViewDelegate{
 
         print (view.annotation?.title)
         
+        
+        
+performSegue(withIdentifier: "sendingParkingId", sender: nil)
+
         //self.navigationController?.pushViewController(ReservationControler, animated: true)
     
     }
