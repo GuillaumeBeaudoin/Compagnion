f3161cac2d25947de7230766f83431df90080f6f
tial1212 <tial1212@gmail.com>
2019-04-16 09:58:09 -0400

    constructor Ajency

--- colval/colval/Agency+CoreDataClass.swift
+++ colval/colval/Agency+CoreDataClass.swift
@@ -12,5 +12,25 @@ import CoreData
 
 @objc(Agency)
 public class Agency: NSManagedObject {
+    
+    public init(pAgencyId : Int16, pAgencyName:Int16,pRoutes:NSSet?) {
+        super.init(entity: NSEntityDescription.entity(forEntityName: "Agency", in: CoreData().context)!, insertInto: CoreData().context)
+        self.agency_id = pAgencyId
+        self.agency_name =  pAgencyName
+        self.routes =  pRoutes 
+    }
+    
+    public init( pAgencyName:Int16,pRoutes:NSSet?) {
+        super.init(entity: NSEntityDescription.entity(forEntityName: "Agency", in: CoreData().context)!, insertInto: CoreData().context)
+        self.agency_name =  pAgencyName
+        self.routes =  pRoutes
+    }
+    
+    
+    @objc
+    private override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    
 
 }
