36a83fe3ea9fa0a53a63452ef59690cbfe8cb9a6
user150454 <user150454@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-05-05 17:53:02 -0500

    MainViewController : Done BusController : Missing ShapeBonus : a faire

--- colval/colval/Controler/ViewController.swift
+++ colval/colval/Controler/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController,  RouteTVControlerListener , CLLocationMa
     private var selectedArrayTrip: [Trips]?  = nil
     private var  uniqueHeadsign:[String] = Array(Set(["Selectioner une ligne"]))
     private var  headsignPos           = 0
-       private var userLocation:CLLocation =  CLLocation(latitude: DataControler.sharedInstance.colValRegion.center.latitude, longitude: DataControler.sharedInstance.colValRegion.center.longitude)
+        private var userLocation:CLLocation =  CLLocation(latitude: DataControler.sharedInstance.colValRegion.center.latitude, longitude: DataControler.sharedInstance.colValRegion.center.longitude)
     
     
     private lazy var routeDataSourceProvider = RouteTVControler(pRouteDataManager: RouteDataManager(pRouteType: RouteDataManager.FAVORITE) , pListener: self )
@@ -71,21 +71,9 @@ class ViewController: UIViewController,  RouteTVControlerListener , CLLocationMa
         self.tableView.dataSource = routeDataSourceProvider
         self.tableView.delegate = routeDataSourceProvider
     }
-    
- 
-    
-   
-
-    
     /*
      * RouteTableViewListener
      */
-    
-    
-    
-    
-    
-    
     func didSelectRoute(pRoute  : Routes!)  {
         
         if let busMapVC2 = UIStoryboard(name: "Main", bundle: nil)
@@ -93,8 +81,22 @@ class ViewController: UIViewController,  RouteTVControlerListener , CLLocationMa
             as? BusMapViewController {
             print("TODO :  inside didSelectRoute ")
         
-        /*btnDestPrev.isEnabled = true
-        btnDestNext.isEnabled = true*/
+            getAllHeadSign(pRoute: pRoute)
+
+
+            busMapVC2.selectedRoute =  pRoute
+            busMapVC2.selectedStop = self.nearestStop
+            print( "nearest :", self.nearestStop)
+            self.navigationController?.pushViewController(busMapVC2, animated: true)
+        }
+            
+    }
+    
+    
+    
+    
+    
+    func getAllHeadSign(pRoute  : Routes!){
         
         var arrayHeadsign:[String] = []
         var arrayCal:[Calender] = []
@@ -108,141 +110,54 @@ class ViewController: UIViewController,  RouteTVControlerListener , CLLocationMa
         }
         self.headsignPos = 0
         self.uniqueHeadsign = Array(Set(arrayHeadsign))
-     //   self.setDayInFunction(pCalender: arrayCal)
         
-        setDirectionButton()
-            
-            busMapVC2.selectedRoute =  pRoute
-            
-            busMapVC2.selectedStop = self.nearestStop
-            print( "nearest :", self.nearestStop)
-            self.navigationController?.pushViewController(busMapVC2, animated: true)
-        }
-            
+        var arrayTrips = CoreData.sharedInstance.getTripFromHeadsign(pHeadsign: uniqueHeadsign[self.headsignPos])
+        
+        self.selectedArrayTrip = arrayTrips!
+        setNearestStop(pArrayTrips: arrayTrips!)
     }
     
-    
-    
-    func setDirectionButton()  {
-        let prevEnabled  =  (     self.headsignPos >    0             )
-        let nextEnabled  =  ( uniqueHeadsign.count-1 > self.headsignPos )
-        
-       
-        let arrayTrips = CoreData.sharedInstance.getTripFromHeadsign(pHeadsign: uniqueHeadsign[self.headsignPos])
-        if (arrayTrips != nil) {
-            setNearestStop(pArrayTrips: arrayTrips!)
-            // TODO FINISH
-            self.selectedArrayTrip = arrayTrips!
-          /* self.btnArrets.isEnabled = true
-            self.lblNearestStopDistance.isEnabled = true*/
-        }
-    }
-    
-    /*
-    func didSelectRoute(pRoute  : Routes!)  {
-        
-       
-        
-        if let busMapVC2 = UIStoryboard(name: "Main", bundle: nil)
-            .instantiateViewController(withIdentifier: "busMap")
-            as? BusMapViewController {
-            print("TODO :  inside didSelectRoute ")
-           // pRoute.trips?.forEach({print ("for each trip in rout print : ", $0)})
-            
-     
-          
-            
-             print("ViewController arrayTrip before: ")
-            
-    
-            
-            var arrayHeadsign:[String] = []
-           
-            self.selectedRoute = pRoute
-            
-            for trip in pRoute.trips! {
-                let trip2  = trip as? Trips
-                arrayHeadsign.append(trip2!.headsign!)
-            }
-            
-            self.headsignPos = 0
-            self.uniqueHeadsign = Array(Set(arrayHeadsign))
-           
-            
-            let arrayTrips = CoreData.sharedInstance.getTripFromHeadsign(pHeadsign: uniqueHeadsign[self.headsignPos])
-            if (arrayTrips != nil) {
-                
-                // TODO FINISH
-               // print("werwerwer : ", arrayTrips!)
-                self.selectedArrayTrip = arrayTrips!
 
-            }
-            
-                print("ViewController arrayTrip after : ", arrayTrips)
-            
-                setNearestStop(pArrayTrips: arrayTrips!)
-            
-    
-            busMapVC2.selectedRoute =  pRoute
-           
-           busMapVC2.selectedStop = self.nearestStop
-             print( "nearest :", self.nearestStop)
-            self.navigationController?.pushViewController(busMapVC2, animated: true)
-        }
-    }
-    
-    
-   */
-    
-    
-    
     
     func setNearestStop(pArrayTrips : [Trips] ) {
-
         
-        DispatchQueue.global(qos: .userInitiated).async {
-            print("setNearestStop")
+        
             var finalArrayStops:[Stops] = []
             
-            
-            print("setNearestStop" , pArrayTrips )
             for trip in pArrayTrips {
-                var stopTimes : [StopTimes] = trip.stoptimes!.toArray()
+                let stopTimes : [StopTimes] = trip.stoptimes!.toArray()
                 for stopTime in stopTimes {
-                    var arrayStops = CoreData.sharedInstance.getStopFrom(pStopTimes: stopTime )
-                    //let stops : [Stops] = stoptimes.stop
+                    let arrayStops = CoreData.sharedInstance.getStopFrom(pStopTimes: stopTime )
+                   
                     
                     for stop in arrayStops! {
                         finalArrayStops.append( stop )
-                       // print("stop.name = \(stop.name!)")
+                        // print("stop.name = \(stop.name!)")
                     }
                 }
             }
             
-            var uniqueStops = Array(Set(finalArrayStops))
-            
+            let uniqueStops = Array(Set(finalArrayStops))
             var arrayLocation:[CLLocation] = []
             for uStop in uniqueStops {
-                //print ("uStop.lat", uStop.lat)
                 arrayLocation.append(CLLocation(latitude: uStop.lat, longitude: uStop.lon))
             }
+            
             var nearestLocation: CLLocation!
             var smallestDistance: CLLocationDistance!
             
             for location in arrayLocation {
                 var distance = self.userLocation.distance(from :location)
                 if smallestDistance == nil || distance < smallestDistance {
-                    
-                    print("distance ",distance)
                     nearestLocation = location
-                   
+                    smallestDistance = distance
                 }
             }
             
-            self.nearestStop =  CoreData.sharedInstance.getStopFrom(pCoordinate: nearestLocation.coordinate)
-            print ("self.nearestStop ", self.nearestStop!)
-  
-        }
+            
+         self.nearestStop =  CoreData.sharedInstance.getStopFrom(pCoordinate: nearestLocation.coordinate)
+     
+        
     }
     
     
