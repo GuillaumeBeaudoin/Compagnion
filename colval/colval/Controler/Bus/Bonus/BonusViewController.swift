9f6d8d6082a3f512d1b0499b288e4928a70e7df2
user150454 <user150454@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-05-09 09:56:44 -0500

    stairs level completed

--- colval/colval/Controler/Bus/Bonus/BonusViewController.swift
+++ colval/colval/Controler/Bus/Bonus/BonusViewController.swift
@@ -13,33 +13,23 @@ class BonusViewController: UIViewController ,UIScrollViewDelegate{
     var scrollView: UIScrollView!
     var imageView: UIImageView!
     
+    var staireIndex = 1
+    @IBOutlet weak var decrease: UIButton!
+    
+    @IBOutlet weak var increase: UIButton!
+    @IBOutlet weak var StaireTitle: UILabel!
+    
+    
+    
+    
+    
+    
+    var STAIRE_LEVELS: [String] = ["SOUS SOL","REZ DE CHAUSSÉE","ÉTAGE 1", "ÉTAGE 2","ÉTAGE 3", "ÉTAGE 4","MEZZANINE-1"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        imageView = UIImageView(image: UIImage(named: "ÉTAGE 2"))
-        
-        
-        scrollView = UIScrollView(frame: view.bounds)
-        
-        scrollView.contentSize = imageView.bounds.size
-        scrollView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.flexibleWidth.rawValue | UIView.AutoresizingMask.flexibleHeight.rawValue)
-        
-        scrollView.addSubview(imageView)
-        view.addSubview(scrollView)
-    
-        
-        scrollView.contentOffset = CGPoint(x: 500, y: 450)
-        scrollView.addSubview(imageView)
-        view.addSubview(scrollView)
-        
-        scrollView.delegate = self
-        scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 4.0
-        scrollView.zoomScale = 1.0
-        setZoomScale()
-        setupGestureRecognizer()
+       
+        setView()
         
         
         
@@ -70,10 +60,8 @@ class BonusViewController: UIViewController ,UIScrollViewDelegate{
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         let imageViewSize = imageView.frame.size
         let scrollViewSize = scrollView.bounds.size
-        
         let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
         let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
-        
         scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
     }
     
@@ -92,6 +80,72 @@ class BonusViewController: UIViewController ,UIScrollViewDelegate{
             scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
         }
     }
+    
+    
+    
+    @IBAction func add(_ sender: Any) {
+        self.staireIndex += 1
+        setView()
+        if (self.staireIndex < 6){
+            decrease.isHidden = false
+        }else
+        {
+          increase.isHidden = true
+        }
+    }
+    
+    
+    
+    @IBAction func remove(_ sender: Any) {
+        self.staireIndex -= 1
+        setView()
+        if (self.staireIndex < 1){
+            decrease.isHidden = true
+        }else{
+            increase.isHidden = false
+        }
+    }
+    
+    func setView() {
+        
+        if ( scrollView != nil) {
+        scrollView.removeFromSuperview()
+        // scrollView.addSubview(UIImageView(image: UIImage(named: STAIRE_LEVELS[staireIndex])))
+        
+        self.scrollView.zoomScale = 1
+        
+        }
+        
+        increase.layer.zPosition = 10;
+        decrease.layer.zPosition = 10;
+        
+        imageView = UIImageView(image: UIImage(named: STAIRE_LEVELS[staireIndex]))
+        StaireTitle.text = STAIRE_LEVELS[staireIndex]
+        
+        scrollView = UIScrollView(frame: view.bounds)
+        
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.flexibleWidth.rawValue | UIView.AutoresizingMask.flexibleHeight.rawValue)
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        
+        
+        scrollView.contentOffset = CGPoint(x: 500, y: 450)
+        scrollView.addSubview(imageView)
+        scrollView.layer.zPosition = -55
+        self.view.addSubview(scrollView)
+        self.view.addSubview(increase)
+        self.view.addSubview(decrease)
+        self.view.addSubview(StaireTitle)
+        self.view.layer.zPosition = -55;
+        
+        
+        setZoomScale()
+        setupGestureRecognizer()
+    }
+    
+
     
     
     
